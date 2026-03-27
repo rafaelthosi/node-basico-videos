4ec96eebc09cdc4d27652b06fba6245aed9585aa
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-03-27 11:59:15 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Hello world'
+})
+
+server.get('/hello', () => {
+    return 'Hello Rocketseat'
+})
+
+server.get('/node', () => {
+    return 'Hello Node.js'
+})
+
+server.listen({
+    port: 3333
+})
