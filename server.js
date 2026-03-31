3da6b72cb0dd398a36e21e7c05cb36164956cb37
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-03-31 11:31:41 -0300

    add: http PUT (memory)

--- server.js
+++ server.js
@@ -23,8 +23,17 @@ server.get('/videos', (request, reply) => {
     return reply.send(videos)
 })
 
-server.put('/videos/:id', () => {
-    return 'Hello Node.js'
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
 })
 
 server.delete('/videos/:id', () => {
