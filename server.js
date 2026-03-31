776adfd89f832c04aa7052a21dbe843475ef4721
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-03-31 11:11:29 -0300

    add: routes.http com teste request post funcionando

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.get('/', () => {
-    return 'Hello world'
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+    database.create({
+        title: 'Video 01',
+        description: 'Esse é o video 01',
+        duration: 100,
+    })
+
+    console.log(database.list())
+
+    return reply.status(201).send()
 })
 
-server.get('/hello', () => {
+server.get('/videos', () => {
     return 'Hello Rocketseat'
 })
 
-server.get('/node', () => {
+server.put('/videos/:id', () => {
+    return 'Hello Node.js'
+})
+
+server.delete('/videos/:id', () => {
     return 'Hello Node.js'
 })
 
