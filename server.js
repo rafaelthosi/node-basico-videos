bf9a9d3e854122d23d53a9ce62173c27f316c563
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-04-01 10:57:42 -0300

    add: delete route

--- server.js
+++ server.js
@@ -36,8 +36,12 @@ server.put('/videos/:id', (request, reply) => {
     return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () => {
-    return 'Hello Node.js'
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
